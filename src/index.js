bb9b0fb00e95a7125fdf2f200e39717d8f84fb9e
diego <diego.maustem@gmail.com>
2024-07-02 17:18:14 -0300

    :tada: init: Executando reactJS

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+const tag = <strong>Olá React</strong>
+
+const el = document.getElementById('root')
+ReactDOM.render( <div> { tag } </div>, el)
+
